bf12fe1c4ed5351ce35c849bd296eca03acc85e3
wtabasum <tbsmnirro@gmail.com>
2023-01-15 14:16:16 -0800

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,27 @@
+# Download archive file, if it does not exist
+
+archiveFile <- "NEI_data.zip"
+
+if(!file.exists(archiveFile)) {
+  
+  archiveURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+  
+  download.file(url=archiveURL,destfile=archiveFile)
+}
+
+if(!(file.exists("summarySCC_PM25.rds") && file.exists("Source_Classification_Code.rds"))) { 
+  unzip(archiveFile) 
+}
+
+# Load the NEI & SCC data frames.
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Plot 1 in the US for 1999, 2995 and 2008
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+
+png('plot1.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
+dev.off()
