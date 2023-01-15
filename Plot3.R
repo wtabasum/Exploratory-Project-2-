dd9b04e218382a65ed4295e47226af76a93cdfd9
wtabasum <tbsmnirro@gmail.com>
2023-01-15 14:15:04 -0800

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,15 @@
+
+# plot 3 
+
+subsetNEI  <- NEI[NEI$fips=="24510", ]
+
+aggregatedTotalByYearAndType <- aggregate(Emissions ~ year + type, subsetNEI, sum)
+
+png("plot3.png", width=640, height=480)
+g <- ggplot(aggregatedTotalByYearAndType, aes(year, Emissions, color = type))
+g <- g + geom_line() +
+  xlab("year") +
+  ylab(expression('Total PM'[2.5]*" Emissions")) +
+  ggtitle('Total Emissions in Baltimore City, Maryland (fips == "24510") from 1999 to 2008')
+print(g)
+dev.off()
